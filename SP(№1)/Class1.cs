feff4f7bc5e6ed53294185868fe6ea9599442ec4
DanilaKUP <attakyhikki777@gmail.com>
2021-03-15 11:03:04 +0300

    Class

--- SP(№1)/Class1.cs
+++ SP(№1)/Class1.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace SP__1_
 {
     class Class1
     {
+        string firm;
+        double diaganal;
+        string tipe;
+
+        public string Firm
+        {
+            get
+            {
+                return firm;
+            }
+            set
+            {
+                this.firm = Firm;
+            }
+        }
+        public string Tipe
+        {
+            get
+            {
+                return tipe;
+            }
+            set
+            {
+                this.tipe = Tipe;
+            }
+        }
+        public double Diaganal
+        {
+            get
+            {
+                return diaganal;
+            }
+            set
+            {
+                this.diaganal = Diaganal;
+            }
+        }
     }
 }
