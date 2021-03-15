54e243ec34f2df5deb8739ca24614a01a58afae3
DanilaKUP <attakyhikki777@gmail.com>
2021-03-15 11:09:45 +0300

    Main

--- SP(№1)/Program.cs
+++ SP(№1)/Program.cs
@@ -6,7 +6,37 @@ namespace SP__1_
     {
         static void Main(string[] args)
         {
-           
+
+            bool flag2 = true;
+            while (flag2)
+            {
+                Console.WriteLine("podzadanie");
+                int choise2 = System.Convert.ToInt32(Console.ReadLine());
+                System.IO.StreamReader sr = new System.IO.StreamReader("Z://Файлы//inputFile.txt");
+
+                Class1 country = new Class1();
+
+                switch (choise2)
+                {
+                    case 1:
+                        {
+                            country.vivod();
+                            country.vivod();
+                            country.vivod();
+                            country.vivod();
+                            Console.WriteLine(country);
+                            break;
+                        }
+                    case 2:
+                        {
+                            flag2 = false;
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
